6041cd2c906d6ecea98c11a5e1601916e6ac2e7f
Noah Berman <noah@wetransfer.com>
2018-02-23 09:49:10 +0100

    Rescue from tweet not found

--- deleter.rb
+++ deleter.rb
@@ -9,22 +9,22 @@ class TweetDeleter
   def initialize
     @tweets = []
   end
-  
+
   def get_tweets(client)
     tweets = client.user_timeline(client.user, {count: 200, include_rts: true})
     raise AllTweetsDeleted, "All tweets deleted!" if tweets.empty?
     @tweets = tweets
   end
-  
+
   def delete_tweets(client, goal)
     raise ArgumentError, "Put a number in deleter() on line 57 - use 0 to delete EVERYTHING." if !goal.is_a?(Integer)
-    
+
     count = client.user.tweets_count
-  
+
     puts "-------------------"
     puts "#{count} tweets remaining until #{goal}"
     puts "-------------------"
-  
+
     if count > goal
       if count - goal < 100
         number_to_delete = count - goal
@@ -36,7 +36,7 @@ class TweetDeleter
     else
       puts "#{goal} tweets deleted"
     end
-    
+
   rescue Twitter::Error::TooManyRequests
     puts "You have exceeded Twitter's rate limit, please relax and try again in 24 hours."
   end
@@ -47,12 +47,14 @@ class TweetDeleter
       tweets_to_delete << e.id
       deletes_to_save << e.text
     end
-    
+
     File.open("deleted_tweets.txt", "a") do |f|
       f.puts(deletes_to_save)
     end
 
     client.destroy_status(tweets_to_delete)
+  rescue Twitter::Error::NotFound
+    puts "Couldn't find a tweet, hopefully continuing"
   end
 
 end
@@ -66,4 +68,4 @@ end
 
 deleter = TweetDeleter.new
 deleter.get_tweets(client)
-deleter.delete_tweets(client, 2)
+deleter.delete_tweets(client, 2 )
